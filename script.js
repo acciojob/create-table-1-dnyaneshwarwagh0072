4829e16fe7316a9627f8e616daedb65308a20bed
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-10 15:57:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function insert_Row() {
-    // Get the table element by its ID
     let table = document.getElementById('sampleTable');
 
-    // Create a new table row element
     let newRow = table.insertRow(0); // Insert at the top (index 0)
-
-    // Create the left cell (New Cell1)
+	
     let leftCell = newRow.insertCell(0);
     leftCell.textContent = 'New Cell1';
 
-    // Create the right cell (New Cell2)
     let rightCell = newRow.insertCell(1);
     rightCell.textContent = 'New Cell2';
 }
